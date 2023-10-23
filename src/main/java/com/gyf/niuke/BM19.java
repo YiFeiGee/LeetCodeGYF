6c6f752e5e8500c374aae93a9a2c10dea42a7525
geyifei <dlgeyifei@gmail.com>
2023-10-23 20:42:06 +0800

    BM19 新解

--- src/main/java/com/gyf/niuke/BM19.java
+++ src/main/java/com/gyf/niuke/BM19.java
@@ -5,7 +5,7 @@ package com.gyf.niuke;
  */
 public class BM19 {
 
-    public int findPeakElement (int[] nums) {
+    public int findPeakElement1 (int[] nums) {
         // write code here
         if ( nums == null) {
             return -1;
@@ -31,5 +31,31 @@ public class BM19 {
         }
         return -1;
     }
+
+
+    // 抓住一个特点，任意一个峰值，就把一个大问题拆成了小问题
+    // 关键在于如何实现这种log2n的时间复杂度，必须可以用条件一次性过滤掉一半数据
+    // 凸峰如果出现，一定是在大的一边，只要右边大，就继续往右找，因为这种情况下一定会出行凸峰
+    // 所以需要用nums[mid]和nums[mid+1]比较
+    public int findPeakElement (int[] nums) {
+        if (nums == null) {
+            return -1;
+        }
+        int l = 0, r = nums.length-1, mid = 0;
+        while (l < r) {
+            mid = (l+r) / 2;
+            // 注意这里mid要保留，不能用mid-1；因为nums[mid]是大的数，很可能这就是一个峰
+            // 题目强调了相邻元素不会相等，其实如果没有这个条件，不能这样直接二分
+            if (nums[mid] > nums[mid+1]) {
+                r = mid;
+            //右边是往上，一定能找到波峰
+            } else {
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+
+
 
 }
